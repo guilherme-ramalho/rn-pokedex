2cfe099dc7128b48317e1c9fe72fa095af46742a
guilherme-ramalho <guilherme-ramalho@outlook.com>
2020-01-21 01:00:00 -0300

    Showing attributes

--- src/screens/PokemonInfo/index.js
+++ src/screens/PokemonInfo/index.js
@@ -12,17 +12,27 @@ import {
   TitleText,
   SectionText,
   BadgeRow,
+  TitleRow,
   TypeBadge,
   TypeText,
+  Col,
+  AttributeText,
+  AttributesContainer,
 } from './styles';
 
 export default function PokemonInfo({ navigation }) {
   const pokemonData = navigation.getParam('pokemonData');
 
+  const upperCaseFirstChar = string =>
+    string[0].toUpperCase() + string.substring(1);
+
+  const randonHexColor = () =>
+    `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+
   const formatDashedString = string => {
     const formatted = string
       .split('-')
-      .map(item => item[0].toUpperCase() + item.substring(1))
+      .map(item => upperCaseFirstChar(item))
       .reduce((holder, currentString) => `${holder} ${currentString}`);
 
     return formatted;
@@ -30,25 +40,46 @@ export default function PokemonInfo({ navigation }) {
 
   return (
     <Container>
+      <TitleRow>
+        <TitleText>{upperCaseFirstChar(pokemonData.name)}</TitleText>
+      </TitleRow>
+      <ImageContainer>
+        <PokemonImage
+          source={{
+            uri: `https://pokeres.bastionbot.org/images/pokemon/${
+              pokemonData.id
+            }.png`,
+          }}
+        />
+      </ImageContainer>
       <ScrollView>
-        <TitleText>{pokemonData.name.toUpperCase()}</TitleText>
-        <ImageContainer>
-          <PokemonImage
-            source={{
-              uri: `https://pokeres.bastionbot.org/images/pokemon/${
-                pokemonData.id
-              }.png`,
-            }}
-          />
-        </ImageContainer>
         <SectionText>Type</SectionText>
         <BadgeRow>
           {pokemonData.types.map(item => (
-            <TypeBadge>
+            <TypeBadge color={randonHexColor()}>
               <TypeText>{item.type.name.toUpperCase()}</TypeText>
             </TypeBadge>
           ))}
         </BadgeRow>
+        <SectionText>Attributes</SectionText>
+        <AttributesContainer>
+          <Col>
+            <AttributeText color="#000">Weight</AttributeText>
+            <AttributeText color="#000">{`${pokemonData.weight /
+              10}kg`}</AttributeText>
+          </Col>
+          <Col>
+            <AttributeText color="#000">Height</AttributeText>
+            <AttributeText color="#000">{`${pokemonData.height /
+              10}m`}</AttributeText>
+          </Col>
+          <Col>
+            <AttributeText color="#000">Base XP</AttributeText>
+            <AttributeText color="#000">
+              {pokemonData.base_experience}
+            </AttributeText>
+          </Col>
+        </AttributesContainer>
         <SectionText>Stats</SectionText>
         <StatsContainer>
           {pokemonData.stats.map(item => {
--- src/screens/PokemonInfo/styles.js
+++ src/screens/PokemonInfo/styles.js
@@ -12,6 +12,17 @@ export const StatsContainer = styled.View`
   border-radius: 5px;
   padding: 10px;
   background-color: white;
+  border: 1px solid #cccccc;
+`;
+
+export const AttributesContainer = styled.View`
+  border-radius: 5px;
+  padding: 10px;
+  background-color: white;
+  border: 1px solid #cccccc;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
 `;
 
 export const StatsBar = styled(Bar)`
@@ -25,16 +36,25 @@ export const ImageContainer = styled.View`
 `;
 
 export const PokemonImage = styled.Image`
-  height: 300px;
-  width: 300px;
+  height: 250px;
+  width: 250px;
 `;
 
 export const TitleText = styled.Text`
-  font-size: 24px;
+  font-size: 28px;
+`;
+
+export const TitleRow = styled.View`
+  flex-direction: row;
+  width: 100%;
+  margin-bottom: 10px;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const SectionText = styled.Text`
   font-size: 20px;
+  margin-top: 10px;
 `;
 
 export const Separator = styled.View`
@@ -47,8 +67,15 @@ export const Separator = styled.View`
 export const BadgeRow = styled.View`
   flex-direction: row;
   width: 100%;
-  margin-bottom: 10px;
   align-items: center;
+`;
+
+export const Col = styled.View`
+  flex-direction: column;
+`;
+
+export const AttributeText = styled.Text`
+  color: ${props => (props.color ? props.color : 'black')};
 `;
 
 export const TypeBadge = styled.View`
@@ -59,6 +86,7 @@ export const TypeBadge = styled.View`
   background-color: ${props => (props.color ? props.color : 'red')};
   align-items: center;
   justify-content: center;
+  margin-right: 10px;
 `;
 
 export const TypeText = styled.Text`
@@ -66,4 +94,4 @@ export const TypeText = styled.Text`
   font-weight: bold;
 `;
 
-export const StatsText = styled.Text``
+export const StatsText = styled.Text``;
